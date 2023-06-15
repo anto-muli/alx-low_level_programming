82e8e55e331e70d491b20dafbd8d744cae8eed74
Anthony Muli <geekytutor2017@gmail.com>
2023-06-16 02:52:41 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main entry
+ * return:always 0 (when sucessful)
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
