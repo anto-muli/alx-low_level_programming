cf9ee3be07e352b3cd852051b92718e5ad13dc60
Anthony Muli <geekytutor2017@gmail.com>
2023-06-19 16:03:50 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a <= 56; a++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (b = 49; b <= 57; b++)
 		{
 			if (b > a)
 			{
 				putchar(a);
 				putchar (b);
-	if (a != 57 || b != 57)
+	if (a != 56 || b != 57)
 	{
 		putchar (',');
 		putchar(' ');
