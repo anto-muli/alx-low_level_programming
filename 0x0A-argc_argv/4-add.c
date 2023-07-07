5e7ba7179eb511066b3f7b7a5bb4821a52ce149c
anto-muli <memuli@students@uonbi.ac.ke>
2023-07-07 19:47:46 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -39,11 +39,15 @@ int main(int argc, char *argv[])
 
 	count = 1;
 
-	while (count < argc) /*Goes through the whole array*/{
-		if (check_num(argv[count])){
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count]))
+		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
-		}else{
+		}
+		else
+		{
 			printf("Error\n");
 			return (1);
 		}
