59cbb08d791904d11b034dbd217feeef98267cd9
anto-muli <memuli@students@uonbi.ac.ke>
2023-08-03 17:37:34 +0300

    3-set_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _atoi(const char *s);
 int _putchar(char c);
 int get_endianness(void);
-
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
