820d994854b49669bd854512c0ace61173601a0c
anto-muli <memuli@students@uonbi.ac.ke>
2023-09-01 18:53:22 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,23 @@
 /**
- * binary_to_uint - converts a binary number to unsigned int
+ * binary_to_uint - converts a bin # to unsigned int
  * @b: points to a string of 0 and 1 chars
  *
- * Return: the converted number or 0 if there is one of morr
+ * Return: the converted # or 0 if there is one of morr
  * chars in the string b that is not 0 or 1 og if b is null
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
+	int a;
 	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (a = 0; b[a]; a++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[a] < '0' || b[a] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[a] - '0');
 	}
 	return (dec_val);
 }
