3cbc40af02e0141533adae6e844bbe01f8183d1c
anto-muli <memuli@students@uonbi.ac.ke>
2023-09-28 14:29:29 +0300

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,23 @@
 /**
  * binary_to_uint - convert a binary # to unsigned int
- * @x: points to a string of 0 and 1 characters
+ * @n: points to a string of 0 and 1 characters
  *
  * Return: the converted # or 0 if there is one of more
  * chars in the string b that is not 0 or 1 og if b is null
  */
-unsigned int binary_to_uint(const char *x)
+unsigned int binary_to_uint(const char *n)
 {
 	int a;
 	unsigned int dec_val = 0;
 
-	if (!x)
+	if (!n)
 		return (0);
 
-	for (a = 0; x[a]; a++)
+	for (a = 0; n[a]; a++)
 	{
-		if (x[a] < '0' || x[a] > '1')
+		if (n[a] < '0' || n[a] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (x[a] - '0');
+		dec_val = 2 * dec_val + (n[a] - '0');
 	}
 	return (dec_val);
 }
