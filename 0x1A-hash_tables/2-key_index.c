86eb83e36bc892ad58bdaf1c995520a0377f866c
anto-muli <memuli@students@uonbi.ac.ke>
2023-12-01 13:06:04 +0300

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,11 +5,12 @@
  * pair should be stored in array of a # table.
  * @key: The key's index
  * @size: The array's size in the hash table.
- * Return: The index of the key.
+ * Return: The index of the key to be returned
  *
  * Description: Uses the djb2 algorithm.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
+
 }
